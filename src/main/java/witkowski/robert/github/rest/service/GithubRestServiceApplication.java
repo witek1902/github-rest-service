f75665f0bc07a89538cd235217f2c346c61a3e85
witek1902 <witek1902@gmail.com>
2017-09-26 17:01:57 +0200

    Main Spring Boot application class.

---
+++ src/main/java/witkowski/robert/github/rest/service/GithubRestServiceApplication.java
@@ -0,0 +1,12 @@
+package witkowski.robert.github.rest.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubRestServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubRestServiceApplication.class, args);
+	}
+}
