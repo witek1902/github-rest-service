bbacd1bb9551b3a580e544bd22686b413bd3e598
witek1902 <witek1902@gmail.com>
2017-09-26 20:46:43 +0200

    Add logger to endpoint.

--- src/main/java/witkowski/robert/github/rest/service/endpoint/GitHubEndpoint.java
+++ src/main/java/witkowski/robert/github/rest/service/endpoint/GitHubEndpoint.java
@@ -1,5 +1,8 @@
 package witkowski.robert.github.rest.service.endpoint;
 
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,21 +18,23 @@ import java.util.Collection;
 
 @RestController
 @RequestMapping("repositories")
+@Slf4j
 class GitHubEndpoint {
-
     @Autowired
     private GhRepositoryService ghRepositoryService;
 
     @RequestMapping(value = "/{owner}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     Collection<GhRepositoryDto> findAllRepositoriesForOwner(@PathVariable String owner) throws IOException {
+        log.info(String.format("findAllRepositoriesForOwner > start. Params: owner = %s", owner));
         Collection<GhRepositoryDto> repos = ghRepositoryService.findAllBy(owner);
         return repos;
     }
 
-    @RequestMapping(value = "/{owner}/{repositoryName}", method =  RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    @RequestMapping(value = "/{owner}/{repositoryName}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     GhRepositoryDto findRepositoryForOwner(@PathVariable String owner, @PathVariable String repositoryName) throws IOException {
+        log.info(String.format("findRepositoryForOwner > start. Params: owner = %s, repositoryName = %s", owner, repositoryName));
         GhRepositoryDto repo = ghRepositoryService.findOne(owner, repositoryName);
-        if(repo.isEmpty()) {
+        if (repo.isEmpty()) {
             throw new RepositoryNotFoundException(owner, repositoryName);
         }
         return repo;
