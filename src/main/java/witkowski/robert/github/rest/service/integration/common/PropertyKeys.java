e9d3184b1619fd7b5695bf44730fe38dc7d29061
witek1902 <witek1902@gmail.com>
2017-09-26 17:02:43 +0200

    PropertyProvider abstraction.

---
+++ src/main/java/witkowski/robert/github/rest/service/integration/common/PropertyKeys.java
@@ -0,0 +1,9 @@
+package witkowski.robert.github.rest.service.integration.common;
+
+
+/**
+ * This class is used for keep property keys. Instead this we could use .properties files and @Value annotations.
+ */
+public class PropertyKeys {
+    public static final String API_HOST = "API_HOST";
+}
---
+++ src/main/java/witkowski/robert/github/rest/service/integration/common/PropertyProvider.java
@@ -0,0 +1,13 @@
+package witkowski.robert.github.rest.service.integration.common;
+
+/**
+ * This interface is responsible for provide static property from resource (file, database, etc.)
+ */
+public interface PropertyProvider {
+    /**
+     * Get property for selected key. If property not exists - method should throws one of RuntimeException.
+     * @param key - Key of property
+     * @return - String with property value.
+     */
+    String getAsString(String key);
+}
