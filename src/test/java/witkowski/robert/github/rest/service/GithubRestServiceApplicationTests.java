8f2c3bf292b3cd652da19b5cb215c50acc913923
witek1902 <witek1902@gmail.com>
2017-09-26 20:49:21 +0200

    Example test with RestTemplate.

--- src/test/java/witkowski/robert/github/rest/service/GithubRestServiceApplicationTests.java
+++ src/test/java/witkowski/robert/github/rest/service/GithubRestServiceApplicationTests.java
@@ -1,16 +1,25 @@
 package witkowski.robert.github.rest.service;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.client.RestTemplate;
+import witkowski.robert.github.rest.service.domain.dto.GhRepositoryDto;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class GithubRestServiceApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    private static final String ONE_REPOSITORY_URL = "http://localhost:8080/repositories/allegro/akubra";
+
+    private RestTemplate restTemplate = new RestTemplate();
+
+    @Test
+    public void callForOneRepository() {
+        GhRepositoryDto dto = restTemplate.getForObject(ONE_REPOSITORY_URL, GhRepositoryDto.class);
+        Assert.assertNotNull(dto);
+    }
 
 }
