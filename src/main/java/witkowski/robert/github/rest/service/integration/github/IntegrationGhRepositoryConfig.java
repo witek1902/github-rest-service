4c3c0c7d63cb39bfeac2a5c9e3d14848d685c806
witek1902 <witek1902@gmail.com>
2017-09-27 17:02:39 +0200

    Configure connectTImeout and readTimeout in HttpClient.

--- src/main/java/witkowski/robert/github/rest/service/integration/github/IntegrationGhRepositoryConfig.java
+++ src/main/java/witkowski/robert/github/rest/service/integration/github/IntegrationGhRepositoryConfig.java
@@ -2,12 +2,15 @@ package witkowski.robert.github.rest.service.integration.github;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import okhttp3.OkHttpClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 import witkowski.robert.github.rest.service.integration.common.PropertyKeys;
 import witkowski.robert.github.rest.service.integration.common.PropertyProvider;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * Integration with GitHub configuration. Create required beans (GSON, PropertyProvider, Retrofit).
@@ -27,9 +30,15 @@ class IntegrationGhRepositoryConfig {
         Gson gson = new GsonBuilder()
                 .create();
 
+        OkHttpClient client = new OkHttpClient.Builder()
+                .readTimeout(15, TimeUnit.SECONDS)
+                .connectTimeout(15, TimeUnit.SECONDS)
+                .build();
+
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl(host)
                 .addConverterFactory(GsonConverterFactory.create(gson))
+                .client(client)
                 .build();
 
         return retrofit.create(GhApiEndpointInterface.class);
